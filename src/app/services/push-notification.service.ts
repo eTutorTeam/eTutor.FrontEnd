d27555b2f09b3df962f28e6755429e54942de4cc
Juan DAniel Ozuna <juandanielozuna2@gmail.com>
2019-12-16 23:52:26 -0400

    Built service to handle actions that come from push notifications

--- src/app/services/push-notification.service.ts
+++ src/app/services/push-notification.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { FirebaseX } from '@ionic-native/firebase-x/ngx';
 import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -9,26 +10,57 @@ export class PushNotificationService {
 
 constructor(
   private firebase: FirebaseX,
-  private toastController: ToastController
-) {
- }
+  private toastController: ToastController,
+  private router: Router
+) {}
 
 public listenWhenUserTapsNotification() {
   console.log('this method was instantiated');
   this.firebase.onMessageReceived().subscribe(data => {
-    console.log('NOTIFICATION', data);
-
+    this.handleNotification(data);
   })
 }
 
-private async presentNotificationToast(notification: any) {
+private async handleNotification(notification: any) {
   if (!notification.tap && notification.tap !== 'background') {
-    const toast = this.toastController.create({
-      message: notification.body,
-      header: notification.title,
-      position: 'top'
-    })
+    await this.showToastOnNotificationIfAppIsActive(notification);
+  } else {
+    await this.handleAppActionDependingOnNotification(notification);
   }
 }
+
+private async showToastOnNotificationIfAppIsActive(notification: any) {
+  const toast = await this.toastController.create({
+    message: notification.body,
+    header: notification.title,
+    position: 'top',
+    buttons: [
+      {
+        side: 'end',
+        icon: 'close',
+        handler: () => {
+          toast.dismiss();
+        }
+      },
+      {
+        side: 'end',
+        icon: 'arrow-forward',
+        handler: async () => {
+          await this.handleAppActionDependingOnNotification(notification);
+        }
+      }
+    ]
+  });
+  toast.present();
+
+  setTimeout(async () => {
+    toast.dismiss();
+    await this.handleAppActionDependingOnNotification(notification);
+  }, 15000);
+}
+
+  private async handleAppActionDependingOnNotification(notification: any) {
+    this.router.navigate(['profile']);
+  }
 
 }
