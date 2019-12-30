99aa636fcdce8e341732c6c33ab700519808c0e0
Juan DAniel Ozuna <juandanielozuna2@gmail.com>
2019-12-29 23:15:24 -0400

    Succesfully sent request to reschedule meeting

--- src/app/students/student-reschedule/student-reschedule-summary/student-reschedule-summary.component.ts
+++ src/app/students/student-reschedule/student-reschedule-summary/student-reschedule-summary.component.ts
@@ -33,18 +33,14 @@ export class StudentRescheduleSummaryComponent implements OnInit {
   ) { }
 
   get startTime() {
-    return moment(this.meeting.startTime).format('LLLL h:mm a');
+    return moment(this.meeting.startTime).format('LLLL');
   }
 
   get endTime() {
-    return moment(this.meeting.endTime).format('LLLL h:mm a');
+    return moment(this.meeting.endTime).format('LLLL');
   }
 
   ngOnInit() {
-    this.loadScreen();
-  }
-
-  ionViewWillEnter() {
     this.loadScreen();
   }
 
@@ -60,19 +56,31 @@ export class StudentRescheduleSummaryComponent implements OnInit {
   }
 
   submitMeeting() {
-
-  }
-
-  private loadScreen() {
-    this.getParams();
-    this.getRequests().catch(err => {
+    this.loadingService.startLoading('Reagendando la tutoría');
+    const meetingId = parseInt(this.meetingId, 10);
+    this.studentMeetingService.rescheduleMeeting(meetingId, this.newTutor.id).then(res => {
+      this.loadingService.stopLoading();
+      this.router.navigate(['home']);
+    }).catch(err => {
       this.loadingService.stopLoading();
       this.toastNotificationService.presentErrorToast(err);
     });
   }
 
+  private loadScreen() {
+    this.getParams();
+    this.getRequests()
+        .then(() => {
+          this.loadingService.stopLoading();
+        })
+        .catch(err => {
+          this.loadingService.stopLoading();
+          this.toastNotificationService.presentErrorToast(err);
+        });
+  }
+
   private async getRequests() {
-    await this.loadingService.startLoading();
+    this.loadingService.startLoading();
     this.meeting = await this.meetingService.getMeetingSummary(parseInt(this.meetingId, 10));
     this.newTutor = await this.tutorsService.getTutorById(parseInt(this.tutorId, 10));
     this.loadingService.stopLoading();
